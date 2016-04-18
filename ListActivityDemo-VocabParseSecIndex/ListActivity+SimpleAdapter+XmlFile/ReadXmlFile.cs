dbccd70602a3ce30d3351d8f4215b6b5ed2ea5f1
Brian Bird <profbird@gmail.com>
2016-04-18 16:40:51 -0700

    still in progress...
    Working on reading and parsing an xml file

--- ListActivityDemo-VocabParseSecIndex/ListActivity+SimpleAdapter+XmlFile/ReadXmlFile.cs
+++ ListActivityDemo-VocabParseSecIndex/ListActivity+SimpleAdapter+XmlFile/ReadXmlFile.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Android.Runtime;
+using System.Xml;
 
 namespace ListActivitySimpleAdapterXmlFile
 {
@@ -32,7 +33,37 @@ namespace ListActivitySimpleAdapterXmlFile
 			vocabList.Add(item3);
 		}
 
+		public void ReadFile()
+		{
+			using (XmlReader reader = XmlReader.Create ("perls.xml")) {
+				while (reader.Read ()) {
+					// Only detect start elements.
+					if (reader.IsStartElement ()) {
+						// Get element name and switch on it.
+						switch (reader.Name) {
+						case "perls":
+						// Detect this element.
+							Console.WriteLine ("Start <perls> element.");
+							break;
+						case "article":
+						// Detect this article element.
+							Console.WriteLine ("Start <article> element.");
+						// Search for the attribute name on this current node.
+							string attribute = reader ["name"];
+							if (attribute != null) {
+								Console.WriteLine ("  Has attribute name: " + attribute);
+							}
+						// Next read will contain text.
+							if (reader.Read ()) {
+								Console.WriteLine ("  Text node: " + reader.Value.Trim ());
+							}
+							break;
+						}
+					}
+				}
+			}
 
+		}
 	}
 }
 
