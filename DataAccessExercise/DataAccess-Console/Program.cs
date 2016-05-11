9371a9dd2a34bb83c2f9468f6cf606ca56c3f294
Brian Bird <birdb@lanecc.edu>
2016-05-11 12:05:31 -0700

    More fixes to today's exercise
    Uncommented some code that was commented out for testing. Added a
    counter for the rows inserted into the dB

--- DataAccessExercise/DataAccess-Console/Program.cs
+++ DataAccessExercise/DataAccess-Console/Program.cs
@@ -15,11 +15,11 @@ namespace DataAccess.DOS
 		{
 			Console.WriteLine ("Hello SQLite-net Data!");
 
-            // parse the spanish-english vocabulary file
-            const int NUMBER_OF_FIELDS = 7;    // The text file will have 3 fields, English word, Spanish word, and part of speech.
+            // parse the stock csv file
+            const int NUMBER_OF_FIELDS = 7;    // The text file will have 7 fields, The first is the date, the last is the adjusted closing price
             TextParser parser = new TextParser(",", NUMBER_OF_FIELDS);     // instantiate our general purpose text file parser object.
             List<string[]> stringArrays;    // The parser generates a List of string arrays. Each array represents one line of the text file.
-            stringArrays = parser.ParseText(File.Open(@"../../../DataAccessDemo/DAL/GoogleStocks.csv",FileMode.Open));     // Open the file as a stream and parse all the text
+            stringArrays = parser.ParseText(File.Open(@"../../../DataAccess-Console/DAL/GoogleStocks.csv",FileMode.Open));     // Open the file as a stream and parse all the text
 
             // We're using a file in Assets instead of the one defined above
             //string dbPath = Directory.GetCurrentDirectory ();
@@ -27,25 +27,30 @@ namespace DataAccess.DOS
 			var db = new SQLiteConnection (dbPath);
 
 			// Create a Stocks table
-			//if (db.CreateTable (Mono.CSharp.TypeOf(Stock)) != 0) 
-			//	db.DropTable<Stock>();
 			if (db.CreateTable<Stock>() == 0)
 			{
 				// A table already exixts, delete any data it contains
-			//	db.DeleteAll<Stock> ();
+				db.DeleteAll<Stock> ();
 			}
 
             // Don't use the first array, it's a header
             stringArrays.RemoveAt(0);
             // Copy the List of strings into our Database
-        //    foreach (string[] stockInfo in stringArrays)
-       //         db.Insert(new Stock() {
-        //            Symbol = "GOOG",
-        //            Name = "Google",
-         //           Date = Convert.ToDateTime(stockInfo[0]),
-         //           ClosingPrice = decimal.Parse(stockInfo[6]) });
+			int pk = 0;
+			foreach (string[] stockInfo in stringArrays) {
+				pk += db.Insert (new Stock () {
+					Symbol = "GOOG",
+					Name = "Google",
+					Date = Convert.ToDateTime (stockInfo [0]),
+					ClosingPrice = decimal.Parse (stockInfo [6])
+				});
+				// Give an update every 100 rows
+				if (pk % 100 == 0)
+					Console.WriteLine ("{0} rows inserted", pk);
+			}
 
 
+			// Just for testing
 			// Read the stock from the database
 			// Use the Get method with a query expression
 			Stock singleItem = db.Get<Stock> (x => x.Name == "Google");
