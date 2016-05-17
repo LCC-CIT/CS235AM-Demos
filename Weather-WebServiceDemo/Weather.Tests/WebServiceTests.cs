69b03e1234efcd3306ec5aaef5e6c86f4efa742b
Brian Bird <profbird@gmail.com>
2016-05-17 15:37:57 -0700

    Big refactor in process!

---
+++ Weather-WebServiceDemo/Weather.Tests/WebServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using Weather.Droid.graphical.weather.gov;
+
+namespace Weather.Tests
+{
+	[TestFixture]
+	public class WebServiceTests
+	{
+		ndfdXML weatherService;
+
+		[SetUp]
+		public void Setup ()
+		{
+			weatherService = new ndfdXML ();
+		}
+
+		
+		[TearDown]
+		public void Tear ()
+		{
+		}
+
+		[Test]
+		public void Pass ()
+		{
+			Console.WriteLine ("testing");
+			Assert.True (true);
+		}
+
+		[Test]
+		public void Inconclusive ()
+		{
+			Console.WriteLine(weatherService.LatLonListZipCode("97405"));
+			Assert.Inconclusive ("Inconclusive");
+		}
+	}
+}
+
