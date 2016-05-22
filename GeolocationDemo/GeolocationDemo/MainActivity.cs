5c941ee34f08f9097f178a0dfbac741f6160b017
Brian Bird <birdb@lanecc.edu>
2016-05-21 21:27:17 -0700

    Updated the project with the latest Geolocation plug-in
    The project with the old component wouldn't build in VS2015, but it
    built and ran in XS. The new plug-in works in both IDEs

--- GeolocationDemo/GeolocationDemo/MainActivity.cs
+++ GeolocationDemo/GeolocationDemo/MainActivity.cs
@@ -1,19 +1,17 @@
-// Demonstration of the Xamarin gelolocaction plug-in (aka component)
+// Demonstration of the Xamarin gelolocaction component
 // Written by Brian Bird, 6/5/13
+// Updated with the latest Xamarin Geolocation plug-in. BB 5/21/16
 
 using System;
 using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Android.OS;
-using Xamarin.Geolocation;
 using System.Threading.Tasks;
+using Plugin.Geolocator;
 
 namespace GeolocationDemo
 {
-	[Activity (Label = "GeolocationDemo", MainLauncher = true)]
+    [Activity (Label = "GeolocationDemo", MainLauncher = true)]
 	public class MainActivity : Activity
 	{
 
@@ -24,10 +22,11 @@ namespace GeolocationDemo
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Note: In project options, Build, Android Application,
-			// check ACCESS_FINE_LOCATION and ACCESS_COARSE_LOCATION
-			// These must be enabled for Geolocation to work
-			var locator = new Geolocator(this) { DesiredAccuracy = 50 };
+            // Note: In project options, Build, Android Application,
+            // check ACCESS_FINE_LOCATION and ACCESS_COARSE_LOCATION
+            // These must be enabled for Geolocation to work
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 50;
 
 			// Get our button and TextView from the layout resource,
 			// and attach an event to it
@@ -36,7 +35,7 @@ namespace GeolocationDemo
 
 			button.Click += delegate {
 				// GetPositionAsynch will either get position information or timeout
-				locator.GetPositionAsync (timeout: 10000)
+				locator.GetPositionAsync (timeoutMilliseconds: 10000)
 				// After getting position info or timing out, execution will continue here
 				// t represents a Task object (GetPositionAsync returns a Task object)
 				.ContinueWith (t =>  
