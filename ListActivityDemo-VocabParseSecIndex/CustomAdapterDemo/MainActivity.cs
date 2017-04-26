5662a09de0b2c6789acd4f57c1992351fad2ca25
Brian Bird <profbird@gmail.com>
2017-04-25 20:59:59 -0700

    Fixed up the new VocabAdapter file with correct using statements

--- ListActivityDemo-VocabParseSecIndex/CustomAdapterDemo/MainActivity.cs
+++ ListActivityDemo-VocabParseSecIndex/CustomAdapterDemo/MainActivity.cs
@@ -1,6 +1,4 @@
 using Android.App;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace CustomAdapterDemo
@@ -27,53 +25,4 @@ namespace CustomAdapterDemo
 			ListAdapter = new VocabAdapter (this, vocab);
 		}
 	}
-
-
-	public class VocabAdapter : BaseAdapter<VocabItem>
-	{
-		VocabItem[] items;
-		Activity context;
-
-		public VocabAdapter(Activity c, VocabItem[] i ): base()
-		{
-			items = i;
-			context = c;
-		}
-
-		public override long GetItemId(int position)
-		{
-			return position;
-		}
-
-		public override VocabItem this[int position]
-		{
-			get { return items [position];}
-		}
-
-		public override int Count
-		{
-			get { return items.Length;}
-		}
-
-		public override View GetView (int position, 
-		                              View convertView, 
-		                              ViewGroup parent)
-		{
-			View view = convertView;
-			if (view == null)
-				view = context.LayoutInflater.Inflate (
-					Android.Resource.Layout.TwoLineListItem,
-					null);
-			view.FindViewById<TextView> (Android.Resource.Id.Text1).Text
-				= items [position].English;
-			view.FindViewById<TextView> (Android.Resource.Id.Text2).Text
-				= items [position].Spanish;
-			return view;
-		}
-	}
 }
-
-
-
-
-
