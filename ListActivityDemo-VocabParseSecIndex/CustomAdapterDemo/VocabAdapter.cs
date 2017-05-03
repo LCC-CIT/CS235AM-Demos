6b7315e3da43870eb34e417bba20c2cad2575bbb
Brian Bird <profbird@gmail.com>
2017-05-03 13:37:35 -0700

    Added comments

--- ListActivityDemo-VocabParseSecIndex/CustomAdapterDemo/VocabAdapter.cs
+++ ListActivityDemo-VocabParseSecIndex/CustomAdapterDemo/VocabAdapter.cs
@@ -9,28 +9,35 @@ namespace CustomAdapterDemo
 		VocabItem[] items;
 		Activity context;
 
+		// The constructor takes a reference to the host Activity and a data array
 		public VocabAdapter(Activity c, VocabItem[] i) : base()
 		{
 			items = i;
 			context = c;
 		}
 
+		// Returns the row id, which is the same as the items array index
 		public override long GetItemId(int position)
 		{
 			return position;
 		}
 
+		// position indexes the array to get the object in that array element
 		public override VocabItem this[int position]
 		{
 			get { return items[position]; }
 		}
 
+		// Returns the count of elements in the array
 		public override int Count
 		{
 			get { return items.Length; }
 		}
 
-		public override View GetView(int position, View convertView,ViewGroup parent)
+		// Used by the ListView to recycyle the View objects used for each row as
+		// the List is scrolled.
+		// ConvertView is the one that just got scrolled off the ListView
+		public override View GetView(int position, View convertView, ViewGroup parent)
 		{
 			View view = convertView;
 			if (view == null)
