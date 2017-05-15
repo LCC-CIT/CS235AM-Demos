d46ed4a91d759bdaa133ab6d2878987b360a5577
Brian Bird <birdb@lanecc.edu>
2017-05-14 20:41:08 -0700

    Changed layout and added up button to child activity

--- RpsFragDemo/RpsDemo.HardCodedFrag/TranslateActivity.cs
+++ RpsFragDemo/RpsDemo.HardCodedFrag/TranslateActivity.cs
@@ -5,7 +5,7 @@ using Android.Widget;
 
 namespace RpsDemo.HardCodedFrag
 {
-	[Activity (Label = "TranslateActivity")]			
+	[Activity (Label = "TranslateActivity", ParentActivity = typeof(MainActivity))]			
 	public class TranslateActivity : Activity
 	{
 		protected override void OnCreate (Bundle bundle)
@@ -13,6 +13,7 @@ namespace RpsDemo.HardCodedFrag
 			base.OnCreate (bundle);
 
 			SetContentView (Resource.Layout.Translate);
+            ActionBar.SetDisplayHomeAsUpEnabled(true);
 
 			var name = Intent.GetStringExtra("hand_position_name");
 			TextView nameTextView = FindViewById<TextView> (Resource.Id.handTextView);
