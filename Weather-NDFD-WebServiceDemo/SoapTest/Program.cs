ee8bd1690f3894d636ef13b0571b577ebbc4c2e4
Brian Bird <birdb@lanecc.edu>
2017-06-05 16:59:27 -0700

    Added a test project

---
+++ Weather-NDFD-WebServiceDemo/SoapTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using SoapTest.ndfdService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoapTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var soapService = new ndfdXMLPortTypeClient();
+            string result = soapService.LatLonListZipCode("97477");
+            //string result = soapService.LatLonListCityNames("1");
+            Console.WriteLine(result);
+        }
+    }
+}
