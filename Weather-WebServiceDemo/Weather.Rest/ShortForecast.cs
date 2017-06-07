8357e99eba131405c2da617abed62bc7783c2665
Brian Bird <birdb@lanecc.edu>
2017-06-07 08:22:37 -0700

    added file

---
+++ Weather-WebServiceDemo/Weather.Rest/ShortForecast.cs
@@ -0,0 +1,15 @@
+﻿namespace Weather.Rest
+{
+    public class ShortForecast
+    {
+        public string Title { get; set; }
+        public string ForecastText { get; set; }
+
+        public override string ToString()
+        {
+            return Title + "\n" + ForecastText;
+        }
+    }
+
+    public enum ForecastFormat { json, xml};
+}
